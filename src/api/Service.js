6134ec9003f52f2ca9490e86bf3a0283f96016d0
Vladislav Tolkachev <tvladick1996@yandex.ru>
2023-07-22 15:03:16 +0300

    22.07

--- src/api/Service.js
+++ src/api/Service.js
@@ -3,17 +3,17 @@ import httpClient from "./client"
 
 export const selectedFilters = async (filters) => {
     let { selectedMetadata, selectedProjects } = filters;
-    return await httpClient.post('/postSelectedFilters', { data: { selectedMetadata: selectedMetadata, selectedProjects: selectedProjects } })
+    return await httpClient.post('/ms/orders', { data: { selectedMetadata: selectedMetadata, selectedProjects: selectedProjects } })
 }
 
 export const getFilterData = async () => {
-    return await httpClient.get('/getFilterData');
+    return await httpClient.get('/filters');
 }
 
 export const downloadConsigmentExcel = async (table) => {
-    return await httpClient.post('/downloadConsigmentExcel', { data: table }, { responseType: 'blob' })
+    return await httpClient.post('/excel/download', { data: table }, { responseType: 'blob' })
 }
 
 export const sendConsigmentBoxBerry = async (table) => {
-    return await httpClient.post('/sendConsigmentBoxBerry', { data: table })
+    return await httpClient.post('/boxberry/consigment', { data: table })
 }
